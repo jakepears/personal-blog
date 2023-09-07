720282c017c92d6b21f4d199b5c20ab9afc37089
DaCodeWiz <jake.pearson.1694@gmail.com>
2023-09-07 15:15:40 -0700

    web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/index.css";
 import { Providers } from "./providers";
 import { cx } from "@/utils/all";
 import { Inter, Lora } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,7 +25,10 @@ export default function RootLayout({
       suppressHydrationWarning
       className={cx(inter.variable, lora.variable)}>
       <body className="text-gray-800 antialiased duration-300 ease-[cubic-beizer(0.465,0.183,0.153,0.946)] dark:bg-[#1c1c1c] dark:text-gray-400 dark:ease-[cubic-beizer(0.465,0.183,0.153,0.946)]">
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <Analytics />
+        </Providers>
       </body>
     </html>
   );
